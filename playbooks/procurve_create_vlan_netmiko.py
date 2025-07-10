ad6e85fe4232e99c8baa5a9f5c711723787bf6df
Juho Joonas Hakalahti <joonas.hakalahti@student.lab.fi>
2025-07-10 10:22:38 +0300

    Update procurve_create_vlan_netmiko.py

--- playbooks/procurve_create_vlan_netmiko.py
+++ playbooks/procurve_create_vlan_netmiko.py
@@ -1,19 +1,26 @@
-#!/usr/bin/env python3
-import sys
 from netmiko import ConnectHandler
+import sys
 
 device = {
-    "device_type": "hp_procurve",
-    "ip": sys.argv[1],
-    "username": sys.argv[2],
-    "password": sys.argv[3],
+  "device_type": "hp_procurve",
+  "ip": sys.argv[1],
+  "username": sys.argv[2],
+  "password": sys.argv[3],
 }
 
+vlan_id = sys.argv[4]
+vlan_name = sys.argv[5]
+
 net_connect = ConnectHandler(**device)
-net_connect.send_command("configure")
-net_connect.send_command("vlan ",sys.argv[4])
-net_connect.send_command("name ",sys.argv[5])
-net_connect.send_command("exit")
-net_connect.send_command("write memory")
+
+config_commands = [
+    f"vlan {vlan_id}",
+    f"name {vlan_name}",
+]
+output = net_connect.send_config_set(config_commands)
+print(output)
+
+net_connect.save_config()  # optional
 print(net_connect.send_command("show vlan"))
+
 net_connect.disconnect()
